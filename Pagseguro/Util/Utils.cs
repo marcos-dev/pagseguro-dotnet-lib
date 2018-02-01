457c282d62dcc0edc925141c5b3303f8bd8af992
marcosspaiva <marckpaulos@hotmail.com>
2018-02-01 00:57:19 -0200

    Classe PagseguroUtils

--- Pagseguro/Util/Utils.cs
+++ Pagseguro/Util/Utils.cs
@@ -8,15 +8,20 @@ namespace RMC.Pagseguro.Util
     internal static class Utils
     {
         /// <summary>
-        /// 
+        /// Forma string para exibir apenas números
         /// </summary>
         /// <param name="number"></param>
-        /// <returns>string</returns>
+        /// <returns>string somente com números</returns>
         public static string OnlyNumbers(string number)
         {
             return Regex.Replace(number, @"[^\d]", "");
         }
 
+        /// <summary>
+        /// Valida o cpf informado
+        /// </summary>
+        /// <param name="cpf"></param>
+        /// <returns>Cpf válido (true) ou inválido (false)</returns>
         public static bool ValidCpf(string cpf)
         {
             if (string.IsNullOrEmpty(cpf))
@@ -38,9 +43,21 @@ namespace RMC.Pagseguro.Util
             cpf == "99999999999") {
                 return false;
             }
-            
 
-            //TODO: Implementar Validações
+            for (int verifier = 0; verifier < 11; verifier++)
+            {
+                int position = 0;
+                int digit = 0;
+                for (digit = 0;  position < verifier; position++)
+                {
+                    digit += cpf[position] * ((verifier + 1) - position);
+                }
+
+                digit = ((10 * digit) % 11) % 10;
+                if (cpf[position] != digit)
+                    return false;
+
+            }
 
             return true;
         }
